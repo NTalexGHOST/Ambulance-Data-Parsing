3a60786bd794195bde614fd706a32d7d9fe37fd6
beresven <beresven@gmail.com>
2022-07-05 16:25:34 +0300

    Some cleaning project and fix volums

--- devops/post-receive.sh
+++ devops/post-receive.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-git --work-tree=/home/user/Ambulance-Data-Parsing --git-dir=/home/user/Ambulance-Data-Parsing.git checkout -f
+git --work-tree=/home/use/Ambulance-Data-Parsing --git-dir=/home/user/Ambulance-Data-Parsing.git checkout -f
 
-mkdir /home/user/parse-result
+# mkdir /home/user/parse-result
+
+echo "Update received, start compile and reboot app"
+
+if [ docker start compile-jar ]; then
+	echo "Start compiling..."
+else
+	echo "Start running and compiling..."
+	docker run -it -v /home/user/ambulance_data_app/server:/Ambulance-Data-Parsing/target --name compile-jar compile-jar:v1
+fi
+
+docker compose reboot
