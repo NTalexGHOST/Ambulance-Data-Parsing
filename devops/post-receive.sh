410620e3666c2577c5c93ffa3bdc98c1c1382262
beresven <beresven@gmail.com>
2022-07-05 18:20:26 +0300

    test9

--- devops/post-receive.sh
+++ devops/post-receive.sh
@@ -8,17 +8,17 @@ echo "|-----------------------------------------------|"
 echo "| Update received, start compile and reboot app |"
 echo "|-----------------------------------------------|"
 
-if [ docker start compile-jar ]; then
+docker start compile-jar
+if [ $? -eq 0 ]; then
 	echo "Start compiling..."
 else
 	echo "Start running and compiling..."
 	docker run -it -v /home/user/ambulance_data_app/server:/Ambulance-Data-Parsing/target --name compile-jar compile-jar:v1
 fi
-
-if [ docker compose -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml  restart ]; then
+docker compose -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml  restart
+if [ $? -eq 0 ]; then
 	echo "Services restat"
 else
 	docker compose -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml up
 fi
-
 docker ps
