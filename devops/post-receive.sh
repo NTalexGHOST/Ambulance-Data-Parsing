677a10ba25a6ba412105d920f7a1081f1b755776
beresven <beresven@gmail.com>
2022-07-06 19:07:30 +0300

    Fix error then application must be restarted but never be started

--- devops/post-receive.sh
+++ devops/post-receive.sh
@@ -7,15 +7,20 @@ echo "---- Update received, start compile and reboot app ----"
 
 
 echo "---- Start compiling ----"
-docker start -a stderr compile-jar
+docker start devops-compile-jar
 if [ $? -eq 0 ]; then
 	echo -n ""
 else
 	echo "---- Create compile container ----"
-	docker run -a stderr -v /home/user/Ambulance-Data-Parsing/target:/Ambulance-Data-Parsing/target -v /home/user/Ambulance-Data-Parsing/:/Ambulance-Data-Parsing --name devops-compile-jar compile-jar
+	docker run --attach stderr -v /home/user/Ambulance-Data-Parsing/target:/Ambulance-Data-Parsing/target -v /home/user/Ambulance-Data-Parsing/:/Ambulance-Data-Parsing --name devops-compile-jar compile-jar
 fi
 echo "---- Compiling complete ----"
 echo "---- Application will be restart ----"
-docker compose -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml restart
+docker compose --attach stderr -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml restart
+if [ $? -eq 0 ]; then
+	echo -n ""
+else
+	docker compose --attach stderr -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml up
+fi
 echo "---- Application is running ----"
 docker ps
