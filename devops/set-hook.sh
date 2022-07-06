6f4ce5fb0ada77c1bc4e0fca7efa0a9f3f1863b5
beresven <beresven@gmail.com>
2022-07-06 20:20:23 +0300

    Fix set-hook

--- devops/set-hook.sh
+++ devops/set-hook.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo -n "Select branch to checkout: "
-read branch
-git checkout $branch -f
 mv /home/user/Ambulance-Data-Parsing/devops/post-receive.sh /home/user/Ambulance-Data-Parsing/.git/hooks/post-receive
 chmod +x /home/user/Ambulance-Data-Parsing/.git/hooks/post-receive
