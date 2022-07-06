0214eacbbc5f0d8ea3c504ead52cd5713b460cb1
beresven <beresven@gmail.com>
2022-07-06 18:17:22 +0300

    Modify post-receive for more info about the process

--- devops/post-receive.sh
+++ devops/post-receive.sh
@@ -2,20 +2,21 @@
 
 git --work-tree=/home/user/Ambulance-Data-Parsing --git-dir=/home/user/Ambulance-Data-Parsing/.git checkout -f
 
-echo "|-----------------------------------------------|"
-echo "| Update received, start compile and reboot app |"
-echo "|-----------------------------------------------|"
 
-docker start compile-jar
+echo "---- Update received, start compile and reboot app ----"
+
+
+echo "---- Start compiling ----"
+docker start -a stderr compile-jar
 if [ $? -eq 0 ]; then
-	echo "---- Start compiling ----"
+
 else
-	echo "---- Start running and compiling ----"
+	echo "---- Create compile container ----"
 	docker run -a stderr -v /home/user/Ambulance-Data-Parsing/target:/Ambulance-Data-Parsing/target -v /home/user/Ambulance-Data-Parsing/:/Ambulance-Data-Parsing --name compile-jar compile-jar
-	echo "---- Compiling complete! ----"
 fi
+echo "---- Compiling complete ----"
 echo "---- Application will be restart ----"
-docker compose -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml restart
+docker compose -a stderr -f /home/user/Ambulance-Data-Parsing/devops/docker-compose.yml restart
 echo "---- Application is running ----"
 # if [ $? -eq 0 ]; then
 # 	echo "---- Services restart ----"
